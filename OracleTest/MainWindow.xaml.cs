b133bdf8d7878a687f4fc57a35b1a1975dcca861
Ivan Karash <karash.www@gmail.com>
2021-07-30 13:42:02 +0300

    Add project files.

---
+++ OracleTest/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Dapper;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OracleTest
+{
+    class Entity
+    {
+        public int ID;
+        public string TYPE;
+        public string APIRESOURCEID;
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDbResult_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            var connectionString = "Data Source=TESTCN;User Id=karash_is;Password=qwe123;";
+            
+            // Прямое подключение (ADO.NET)
+            //using (OracleConnection connection = new OracleConnection(connectionString))
+            //{
+            //    connection.Open();
+
+            //    OracleCommand command = new OracleCommand();
+            //    command.Connection = connection;
+            //    command.CommandText = "select * from CN.IS4$APIRESOURCECLAIM";
+            //    command.CommandType = CommandType.Text;
+
+            //    OracleDataReader dataReader = command.ExecuteReader();
+            //    dataReader.Read();
+
+            //    lblDbResult.Content = dataReader.GetString(1);
+
+            //    connection.Close();
+            //}
+
+            // Через Dapper
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                connection.Open();
+
+                var entities = connection.Query<Entity>("select ID, TYPE, APIRESOURCEID from CN.IS4$APIRESOURCECLAIM").ToList();
+
+                lblDbResult.Content = entities[8].TYPE.ToString();
+               
+                connection.Close();
+            }
+
+            
+
+
+
+
+        }
+    }
+}
